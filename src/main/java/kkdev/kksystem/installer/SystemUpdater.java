182d15181fe89dbde9f3804bcdcf84beb37f7319
Garikk <supergarikk@gmail.com>
2015-07-13 17:33:14 +0300

    CE Reader and CE Error clean emulation

---
+++ src/main/java/kkdev/kksystem/installer/SystemUpdater.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kkdev.kksystem.installer;
+
+/**
+ *
+ * @author blinov_is
+ */
+public abstract class SystemUpdater {
+    final String DEF_CONFIG="";
+    final String DEF_SITE="";
+    final String DEF_PLUGINS_REP="";
+    
+
+    /*
+       1) Get defaut (or user) config
+       2) Get Required plugins
+       3) Get Plugin configurations (or default)
+    */
+    
+    
+    
+    public static void SelfInstall()
+    {
+    
+    
+    
+    
+    }
+    
+}
